fc9252a1cd66c0bf359920cc4ab8d1cc41861a91
LeCanardNoir <dev.lecanardnoir@gmail.com>
2023-11-27 13:40:48 -0500

    INIT: Project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿// unordered_multiset.cpp : définit le point d'entrée de l'application.
+//
+
+#include "unordered_multiset.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
